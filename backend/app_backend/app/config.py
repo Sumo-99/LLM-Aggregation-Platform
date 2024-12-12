34ec84b6faeb7443587b7446cc447085d45e8510
Sumo-99 <sumanthssr@gmail.com>
2024-12-11 23:12:13 -0500

    working changes

--- backend/app_backend/app/config.py
+++ backend/app_backend/app/config.py
@@ -2,7 +2,7 @@ class Settings:
     APP_NAME = "Model Service"
     VERSION = "1.0.0"
     MODEL_API_MAP = {
-        "gemini": "chat_gemini",
+        "Gemini": "chat_gemini",
         "openai": "chat_openai"
     }
 
